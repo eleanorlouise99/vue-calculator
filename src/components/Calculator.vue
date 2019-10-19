d02d76775d8bd84d337bb8a926b7e679eb2bad0a
Eleanor Morgan <eleanorlouise99@gmail.com>
2019-10-19 15:17:22 +0100

    commit

--- src/components/Calculator.vue
+++ src/components/Calculator.vue
@@ -1,16 +1,49 @@
 <template>
     <div class="calculator">
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
-        <div>hello</div>
+        <div class="display">{{current || '0'}}</div>
+
+        <div @click="clear" class="btn">AC</div>
+        <div @click="sign" class="btn">+/-</div>
+        <div class="btn">%</div>
+        <div class="btn operator">÷</div>
+
+        <div class="btn">7</div>
+        <div class="btn">8</div>
+        <div class="btn">9</div>
+        <div class="btn operator">x</div>
+
+        <div class="btn">4</div>
+        <div class="btn">5</div>
+        <div class="btn">6</div>
+        <div class="btn operator">-</div>
+
+        <div class="btn">1</div>
+        <div class="btn">2</div>
+        <div class="btn">3</div>
+        <div class="btn operator">+</div>
+
+        <div class="btn zero">0</div>
+        <div class="btn">.</div>
+        <div class="btn operator">=</div>
     </div>
 </template>
 
 <script>
 export default {
-    
+    data() {
+        return {
+            current: '', 
+        }
+    },
+    methods: {
+        clear() {
+            this.current = '';
+        },
+        sign() {
+            this.current = this.current.charAt(0) === '-' ?
+            this.current.slice(1) : `-${this.current}`;
+        }
+    }
 }
 </script>
 
@@ -19,5 +52,28 @@ export default {
     grid-template-columns: repeat(4, 1fr);
     grid-auto-rows: minmax(50px, auto);
     display: grid;
+    font-size: 40px;
+    width: 400px;
+    margin: 0 auto;
+}
+
+.display {
+    grid-column: 1 / 5;
+    background-color: #333;
+    color: white;
+}
+
+.zero {
+    grid-column: 1 / 3;
+}
+
+.btn {
+    background-color: #eee;
+    border: 1px solid #999;
+}
+
+.operator {
+    background-color: orange;
+    color: white;
 }
 </style>
